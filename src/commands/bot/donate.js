4b1464533c2fffcb2e7a993f2aaccd3a30b2e93f
YoleeR1 <Tonyghobrial2008@gmail.com>
2025-03-08 12:09:31 +0000

    Updated the non functional donate.js command

--- src/commands/bot/donate.js
+++ src/commands/bot/donate.js
@@ -4,19 +4,17 @@ module.exports = async (client, interaction, args) => {
     let row = new Discord.ActionRowBuilder()
         .addComponents(
             new Discord.ButtonBuilder()
-                .setLabel("")
-                .setURL("")
+                .setLabel("Donate")
+                .setURL("https://paypal.me/NocturnalsBusiness")
                 .setStyle(Discord.ButtonStyle.Link),
         );
-
+    
     client.embed({
         title: `${client.user.username}・Donate`,
-        desc: '_____ \n\nClick the button below to support the server!\n**All money goes back to the server like giveaway and such**',
+        desc: 'Click the button below to support the server!\n**All money goes back to the server e.g for giveaways**',
         thumbnail: client.user.avatarURL({ dynamic: true }),
-        url: "paypal.me/NocturnalsBusiness",
+        url: "https://paypal.me/NocturnalsBusiness",
         components: [row],
         type: 'editreply'
     }, interaction)
 }
-
- 
