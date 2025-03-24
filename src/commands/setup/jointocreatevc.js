b701b58f2e18b57c3e01668b3a3dc3da91fe2bc3
YoleeR1 <Tonyghobrial2008@gmail.com>
2025-03-24 18:10:32 +0000

    added a jointocreate vc feature!

---
+++ src/commands/setup/jointocreatevc.js
@@ -0,0 +1,7 @@
+// Remove this file if not required, or ensure it contains valid code
+module.exports = {
+    name: 'jointocreatevc',
+    execute: () => {
+        // This subcommand is handled directly in setup.js
+    },
+};
