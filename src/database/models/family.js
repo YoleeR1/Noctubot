8d2975c4d226a0bba5009bb7eb38bd0ea2998db3
YoleeR1 <Tonyghobrial2008@gmail.com>
2025-03-26 06:05:41 +0000

    updated to be able to handle the reworked family commands

--- src/database/models/family.js
+++ src/database/models/family.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const Schema = new mongoose.Schema({
     Guild: String,
     User: String,
-    Parent: { type: Array, default: null },
-    Partner: { type: String, default: null },
-    Children: { type: Array, default: null },
+    Partner: String,
+    Parent: [String], // Parents of the user
+    Children: [String], // Children directly associated with this user
+    SharedChildren: [String], // Children shared with a partner
 });
 
 module.exports = mongoose.model("family", Schema);
